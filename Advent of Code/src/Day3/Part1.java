eb5e91e505164e4bbadaa078b01e123adfd5c854
1909959 <1909959@01-18138.pusd.dom>
2024-12-12 11:27:00 -0800

    day3 stuff

--- Advent of Code/src/Day3/Part1.java
+++ Advent of Code/src/Day3/Part1.java
@@ -11,13 +11,56 @@ public class Part1 {
 		File file = new File("Advent of Code Day 3 input");
 		Scanner scan = new Scanner(file);
 		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list2 = new ArrayList<String>();
+		int index=0;
+		int index2 = 2;
+		int index3 = 1;
 		while(scan.hasNext()) {
 			list.add(scan.next());
 			System.out.println(list);
-			String[] list2 = (String[]) list.toArray();
-			System.out.println(list2);
+			
 		}
 		
-		
+		for(int i = 0;i<list.size();i++) {
+			String obj = list.get(i);
+			ArrayList<String> obj2 = new ArrayList<String>();
+			for(int j = 0;j<obj.length();j++) {
+				obj2.add(obj.substring(j,j+1));
+			}
+			System.out.println(obj2);
+			for(int k = 0;k<obj2.size();k++) {
+				String str = obj2.get(k);
+				if(!(str.equals("m")||str.equals("u")||str.equals("l")||str.equals("(")||str.equals(",")||str.equals(")")||str.equals("1")||str.equals("2")||str.equals("3")||str.equals("4")||str.equals("5")||str.equals("6")||str.equals("7")||str.equals("8")||str.equals("9")||str.equals("0"))) {
+					obj2.remove(k);
+				}
+			}
+			System.out.println(obj2);
+//			if(obj.contains("mul(") && obj.contains(")") && obj.contains(",")) {
+//				index = obj.indexOf("mul(");
+//				index2 = obj.indexOf(")");
+//				index3 = obj.indexOf(",");
+//				if(index<index2 && index<index3 && index2>index3) {
+//					System.out.println(obj.substring(index,index2+1));
+//					System.out.println(obj.length());
+//					list2.add(obj.substring(index,index2+1));
+//				}
+//			}
+//			if(obj2.contains("m") && obj2.contains(")") && obj2.contains("u") && obj.contains(",")) {
+//				index = obj2.lastIndexOf(")");
+//				index3 = obj.lastIndexOf(",");
+//				index4
+//				index2 = obj2.lastIndexOf("m");
+//				if(index-index2<=11 && index-index2>=7) {
+//					for(int g = index2;g<index+1;g++) {
+//						System.out.print(obj2.get(g));
+//						list2.add(obj2.get(g));
+//					}
+//					System.out.println("");
+//			//System.out.println(list2);
+//				}
+//			}
+			
+			
+		}
 	}
 }
